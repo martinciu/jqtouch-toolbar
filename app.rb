8aed366305510511366e5431143a629ba497fe26
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2010-04-10 14:44:45 +0200

    - toolbar icons

--- app.rb
+++ app.rb
@@ -6,14 +6,29 @@ get '/' do
   haml :index
 end
 
-get '/toolbar' do
-  haml :toolbar
-end
-
-get '/toolbar2' do
-  haml :toolbar2
+get '/home' do
+  haml :home
 end
 
 get '/notoolbar' do
   haml :notoolbar
+end
+
+get '/search' do
+  haml :search
+end
+
+post '/search' do
+  unless params[:q].empty?
+    haml :results
+  else
+    @error = "Wpisz coś ziom!"
+    haml :search
+  end
+end
+
+get '/:page' do
+  @page = params[:page]
+  @items = 100.times.map {|n| [@page, n].join(" ")}
+  haml :list
 end
